982806a64a6240ac8caf04e121183bbacd9737c5
jjfiv <jjfivel@gmail.com>
2020-12-17 23:45:37 +0100

    Connection String

--- DataService/Services/Mapping/PostgresSQL_Connect_String.cs
+++ DataService/Services/Mapping/PostgresSQL_Connect_String.cs
@@ -2,11 +2,15 @@
 {
     public class PostgresSQL_Connect_String
     {
-        private string host = "localhost";
-        private string db = "imdb";
-        private string UserId = "jesper";
-        private string pwd = "Hcn27wzv";
+        // private string host = "localhost";
+        // private string db = "imdb";
+        // private string UserId = "postgres";
+        // private string pwd = "";
 
+        private string host = "rawdata.ruc.dk";
+        private string db = "raw5";
+        private string UserId = "raw5";
+        private string pwd = "Aji2O4oD";
         public override string ToString()
         {
             return $"host={host};db={db};uid={UserId};pwd={pwd}";
